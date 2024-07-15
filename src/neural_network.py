d9b858f65445428f5711ecc1b0ec91fa5a729377
xektorise <th.koletzek@gmail.com>
2024-07-15 14:49:27 +0200

    first try of a neural network

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,18 +1,26 @@
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow import keras
+from keras.src.models import Sequential
+from keras.src.layers import Dense
 
 
 class NeuralNetwork:
     def __init__(self):
-        pass
+        self.model = None
     
-    def build_model(self):
-        pass
+    def build_model(self, input, output):
+        self.model = Sequential[(
+            Dense(64, input = input, activation='relu'),
+            Dense(64, activation='relu'),
+            Dense(output, activation='softmax')
+        )]
+        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     
-    def train_model(self):
-        pass
+    def train_model(self, X_train, Y_train, epochs=10, batch_size=32):
+        self.model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size)
     
-    def predict(self):
-        pass
+    def predict(self, X):
+        return self.model.predict(X)
     
